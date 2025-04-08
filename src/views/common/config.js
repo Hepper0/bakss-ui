c46ccfc226d562f31ae198bda3fc70523da3d7dd
余显光 <807657129@qq.com>
2025-04-09 00:00:02 +0800

    新增修改owner与修改管理员申请类型

--- src/views/common/config.js
+++ src/views/common/config.js
@@ -8,7 +8,9 @@ export const MODIFY_DIRECTORY = 6
 export const ENABLE_STRATEGY = 7
 export const DISABLE_STRATEGY = 8
 export const DELETE_STRATEGY = 9
-export const APPLY_TYPE = ['申请备份管理权', '授权备份管理权','恢复创建','备份创建','立即备份','定时备份','修改备份目录','启用备份','策略禁用','策略删除']
+export const MODIFY_OWNER = 10
+export const MODIFY_MANAGER = 11
+export const APPLY_TYPE = ['申请备份管理权', '授权备份管理权','恢复创建','备份创建','立即备份','定时备份','修改备份目录','启用备份','策略禁用','策略删除', '修改owner', '修改管理员']
 
 export const TYPE_BACKUP_PERMISSION = 0
 export const TYPE_CREATE_RESTORE = 1
@@ -16,13 +18,15 @@ export const TYPE_CREATE_BACKUP = 2
 export const TYPE_BACKUP_ONCE = 3
 export const TYPE_MODIFY_DIRECTORY = 4
 export const TYPE_STRATEGY = 5
+export const TYPE_MODIFY_USER = 6
 export const APPLY_MAP = [
   [APPLY_BACKUP_PERMISSION, GRANT_BACKUP_PERMISSION],
   [CREATE_RESTORE],
   [CREATE_BACKUP],
   [BACKUP_RIGHT_NOW, BACKUP_AT_TIME],
   [MODIFY_DIRECTORY],
-  [ENABLE_STRATEGY, DISABLE_STRATEGY, DELETE_STRATEGY]
+  [ENABLE_STRATEGY, DISABLE_STRATEGY, DELETE_STRATEGY],
+  [MODIFY_OWNER, MODIFY_MANAGER]
 ]
 
 export const TASK_STATUS = ['指派人审批', '客户端owner审批', 'dba审批', '备份管理员审批']
