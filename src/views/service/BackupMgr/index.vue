8061a231b07dcda2d7f0de42315fd7f07f7692b0
余显光 <hepper.yu@igg.com>
2025-05-19 20:05:22 +0800

    新增备份同步弹窗

--- src/views/service/BackupMgr/index.vue
+++ src/views/service/BackupMgr/index.vue
@@ -1,5 +1,92 @@
 <template>
   <div class="backup-management">
+    <el-dialog
+      title="备份任务同步"
+      :visible.sync="syncBackupJobVisible"
+    >
+      <div>
+        <el-table>
+          <el-table-column prop="name" label="任务名称"></el-table-column>
+          <el-table-column prop="type" label="任务类型"></el-table-column>
+          <el-table-column prop="description" label="任务描述"></el-table-column>
+          <el-table-column prop="repositoryName" label="仓库名称"></el-table-column>
+          <el-table-column prop="scheduleEnabled" label="是否启用"></el-table-column>
+          <el-table-column prop="creationDateUtc" label="创建时间"></el-table-column>
+        </el-table>
+      </div>
+      <div>
+        <el-card class="card-panel">
+          <div slot="header" class="clearfix">
+            <span>基本信息</span>
+          </div>
+          <div class="card-panel-content">
+            <el-form ref="basicForm" :model="basicFormData" :rules="basicRules" size="medium" label-width="120px">
+              <el-col :span="8">
+                <el-form-item label="备份内容" prop="backupContent">
+                  <el-select v-model="basicFormData.backupContent" :style="{width: '80%'}" @change="onContentChange">
+                    <el-option v-for="(item, index) in backupContentOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="机器类型" prop="machineType" label-width="120px">
+                  <el-select v-model="basicFormData.machineType" placeholder="请输入任务名称" :disabled="cascadeRule && cascadeRule['machineType'] !== undefined" :style="{width: '80%'}">
+                    <el-option v-for="(item, index) in machineTypeOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="数据中心" prop="dataCenter">
+                  <el-select v-model="basicFormData.dataCenter" :style="{width: '80%'}">
+                    <el-option v-for="(item, index) in dataCenterOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="环境" prop="env">
+                  <el-select v-model="basicFormData.env" :style="{width: '80%'}">
+                    <el-option v-for="(item, index) in envOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="操作系统类型" prop="platform" label-width="120px">
+                  <el-select v-model="basicFormData.platform" :style="{width: '80%'}" :disabled="cascadeRule && cascadeRule['platform'] !== undefined">
+                    <el-option v-for="(item, index) in platformOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="备份软件" prop="backupSoftware" :disabled="cascadeRule && cascadeRule['backupSoftware'] !== undefined">
+                  <el-select v-model="basicFormData.backupSoftware" :style="{width: '80%'}">
+                    <el-option v-for="(item, index) in backupSoftwareOptions" :key="index" :label="item.label" :value="item.value" />
+                  </el-select>
+                </el-form-item>
+              </el-col>
+            </el-form>
+          </div>
+        </el-card>
+        <el-card class="card-panel">
+          <div slot="header" class="clearfix">
+            <span>操作系统</span>
+          </div>
+          <div class="card-panel-content">
+            <el-form ref="platformForm" :model="platformFormData" :rules="platformRules" size="medium" label-width="120px">
+              <el-col :span="8">
+                <el-form-item label="备份IP" prop="backupIP">
+                  <el-input v-model="platformFormData.backupIP" placeholder="请输入" :style="{width: '80%'}"/>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="远程端口" prop="backupPort">
+                  <el-input v-model="platformFormData.backupPort" placeholder="请输入" :style="{width: '80%'}"/>
+                </el-form-item>
+              </el-col>
+            </el-form>
+          </div>
+        </el-card>
+      </div>
+    </el-dialog>
     <div class="panel-container">
       <!-- 搜索栏 -->
       <el-row :gutter="10">
@@ -16,6 +103,7 @@
           <el-button size="mini" @click="reset" icon="el-icon-refresh">重置</el-button>
           <el-button size="mini" @click="createBackup" type="success">创建备份</el-button>
           <el-button size="mini" @click="gotoApply">申请备份管理员权限</el-button>
+          <el-button size="mini" @click="syncBackupJob">同步备份任务</el-button>
         </el-col>
       </el-row>
     </div>
@@ -135,10 +223,78 @@ import { myBackup } from "@/api/service/backup"
 import { listJob } from "@/api/veeam/job"
 import QueryCondition from "@/components/QueryCondition"
 
+const basicRules = {
+  backupContent: [{
+    required: true,
+    message: '请选择备份内容',
+    trigger: 'blur'
+  }],
+  machineType: [{
+    required: true,
+    message: '请选择机器类型',
+    trigger: 'blur'
+  }],
+  dataCenter: [{
+    required: true,
+    message: '请选择数据中心',
+    trigger: 'blur'
+  }],
+  env: [{
+    required: true,
+    message: '请选择环境',
+    trigger: 'blur'
+  }],
+  platform: [{
+    required: true,
+    message: '请选择操作系统',
+    trigger: 'blur'
+  }],
+  backupSoftware: [{
+    required: true,
+    message: '请选择备份软件',
+    trigger: 'blur'
+  }],
+}
+const platformRules = {
+  backupIP: [{
+    required: true,
+    message: '请输入',
+    trigger: 'blur'
+  }],
+  backupPort: [{
+    required: true,
+    message: '请输入',
+    trigger: 'blur'
+  }],
+}
+
+const backupContentOptions = [
+  {label: 'VMware', value: 'VMware'},
+  {label: 'MySQL', value: 'MySQL'},
+  {label: 'SQL Server', value: 'SQL Server'},
+  {label: 'PostgreSQL', value: 'PostgreSQL'},
+  {label: 'Oracle', value: 'Oracle'},
+  {label: 'FileSystem', value: 'FileSystem'},
+]
+
+const dataCenterOptions = [{label: 'CATL/宁德时代', value: 'catl'}]
+const platformOptions = [{label: 'Windows', value: 'Windows'}, {label: 'Linux', value: 'Linux'}]
+const envOptions = [{label: '生产', value: 'prod'}, {label: '非生产', value: 'dev'},]
+const backupSoftwareOptions = [{label: 'Veeam', value: 'Veeam'}]
+const machineTypeOptions = [{label: '虚拟机', value: 'vm'}, {label: '物理机', value: 'physical'},]
+
 export default {
   name: 'BackupMgr',
   data() {
     return {
+      basicRules,
+      platformRules,
+      backupContentOptions,
+      dataCenterOptions,
+      platformOptions,
+      envOptions,
+      backupSoftwareOptions,
+      machineTypeOptions,
       open: false,
       loading: false,
       total: 0,
@@ -154,7 +310,20 @@ export default {
       ],
       selectedRows: [],
       form: {},
-      rules: {}
+      rules: {},
+      syncBackupJobVisible: false,
+      basicFormData: {
+        backupContent: undefined,
+        machineType: undefined,
+        dataCenter: undefined,
+        backupSoftware: undefined,
+        platform: undefined,
+        env: undefined
+      },
+      platformFormData: {
+        backupIP: undefined,
+        backupPort: 22
+      },
     };
   },
   components: {
@@ -236,6 +405,9 @@ export default {
     },
     cancel() {
       this.open = false
+    },
+    syncBackupJob() {
+      this.syncBackupJobVisible = true
     }
   }
 };
